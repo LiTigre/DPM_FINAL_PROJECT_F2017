ec01e67a3f0830769ea8b97070d40f1f5ed7602c
Moose <mostafa.saadat@outlook.com>
2017-11-09 14:06:19 -0500

    Finished first version of zipline

--- src/ca/mcgill/ecse211/navigation/Zipline.java
+++ src/ca/mcgill/ecse211/navigation/Zipline.java
@@ -19,28 +19,43 @@ public class Zipline {
 	private EV3LargeRegulatedMotor ZiplineMotor;
 	
 	/**
-	 * Constant that indicates that the robot is off the floor (compared to the light sensor data)
+	 * Light sensor value of the floor
 	 */
-	private static final double FLOOR_THRESHOLD;
+	private double FLOOR_INTENSITY;
 	/**
 	 * Speed of the zipline motor in degrees/second
 	 */
-	private static final int ZIPLINE_SPEED = 200;
-
+	private final int ZIPLINE_SPEED = 200;
+	/**
+	 * Create a range of acceptable values to consider the floor
+	 */
+	private final int RANGE = 50;
+	/**
+	 * Number of times same light sensor value has to be read to be considered acceptable
+	 */
+	private final int FILTER = 3;
+	/**
+	 * Keep track of how many times the same value is read from the light sensor
+	 */
+	private int count = 0;
+	
 	/**
 	 * Constructor for the Zipline class.
 	 * @param odometer Odometer created in MainController.
 	 * @param ZiplineMotor Motor controlling the pulley
 	 * @since 1.1
+	 * @version 1.1
 	 */
 	public Zipline(Odometer odometer, EV3LargeRegulatedMotor ZiplineMotor) {
 		this.odometer = odometer;
 		this.ZiplineMotor = ZiplineMotor;
+		this.FLOOR_INTENSITY = calculateFloorIntensity();
 	}
 	
 	/**
 	 * Starts and stops the pulley's motor. 
 	 * @since 1.1
+	 * @version 1.0
 	 */
 	public void performZiplineTravel() {
 		ZiplineMotor.setSpeed(ZIPLINE_SPEED);
@@ -57,27 +72,32 @@ public class Zipline {
 	 * on ground
 	 * @return boolean true if finished traversing zipline, otherwise false
 	 * @since 1.2
+	 * @version 1.1
 	 */
 	private boolean hasLanded() {
-		if (MainController.getLightValue() > FLOOR_THRESHOLD) return true;
+		if (MainController.getLightValue() < (FLOOR_INTENSITY + RANGE) && MainController.getLightValue() > (FLOOR_INTENSITY - RANGE)) {
+			if (count > FILTER) return true;
+			count++;
+		} else {
+			count = 0;
+		}
 		return false;
 	}
 	
 	/**
-	 * Return the data retrieved from the color sensor
-	 * @return float data from the color sensor
+	 * Return the intensity of the floor calculated by getting the average
+	 * of the values from the light sensor when facing the floor
+	 * @return float light intensity of floor
 	 * @since 1.2
+	 * @version 1.1
 	 */
 	
-	private float calculateFloorColor() {
-		ArrayList<Float> colors = new ArrayList<Float>();
+	private float calculateFloorIntensity() {
 		float sum = 0;
-		float color;
-		for (int i = 0; i < 1000; i++) {
-			color = getColor();
-			
-			colors.add(getColor());
+		int i;
+		for (i = 0; i < 1000; i++) {
+			sum += MainController.getLightValue();
 		}
-		
+		return sum / i;
 	}
 }
